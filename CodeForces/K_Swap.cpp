ae1152b6f85f5b5ec42a326b6d385c09832fee84
momentum24 <mohamedalim559@gmail.com>
2024-04-26 03:50:37 +0300

    sheet 5

---
+++ CodeForces/K_Swap.cpp
@@ -0,0 +1,89 @@
+/*                                              بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم
+                    قَالَ رَبِّ اشْرَحْ لِي صَدْرِي (25) وَيَسِّرْ لِي أَمْرِي (26) وَاحْلُلْ عُقْدَةً مِّن لِّسَانِي (27) يَفْقَهُوا قَوْلي             */
+
+#include <bits/stdc++.h>
+#define ln "\n"
+typedef  long long ll;
+using namespace std;
+
+// const long long Max = 1e8+5;
+// const int shift = 1e6;
+// ll prefix[Max];
+// queue<int> q;
+
+void solve() {
+    int n,mx = WINT_MIN,mn = WINT_MAX , a,b; cin>>n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+        if(mx < arr[i]) {
+            a=i;
+            mx = arr[i];
+        }
+        if(mn > arr[i]) {
+            b=i;
+            mn = arr[i];
+        }
+    }
+    swap(arr[a] , arr [b]);
+    for(int i =0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+}
+
+
+
+
+
+void Main() {
+    int t = 1;
+    // cin>>t;
+    while(t--) {
+        solve();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cout << fixed << setprecision(1);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("errors.txt", "w", stderr);
+#endif
+    //--------------------
+        // int t;cin>>t;
+    Main();
+}
---
+++ CodeForces/L_Colourblindness.cpp
@@ -0,0 +1,85 @@
+/*                                              بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم
+                    قَالَ رَبِّ اشْرَحْ لِي صَدْرِي (25) وَيَسِّرْ لِي أَمْرِي (26) وَاحْلُلْ عُقْدَةً مِّن لِّسَانِي (27) يَفْقَهُوا قَوْلي             */
+
+#include <bits/stdc++.h>
+#define ln "\n"
+typedef  long long ll;
+using namespace std;
+
+// const long long Max = 1e8+5;
+// const int shift = 1e6;
+// ll prefix[Max];
+// queue<int> q;
+
+void solve() {
+    int x;cin>>x;
+    string c1,c2;
+    cin>>c1>>c2;
+    for(int i=0;i<x;i++) {
+        if( (c1[i]==c2[i] ) || (c1[i]=='G' && c2[i]=='B') || (c1[i]=='B' && c2[i]=='G') ) {
+
+        }
+        else {
+            cout<<"NO"<<ln;
+            return;
+        }
+    }
+    cout<<"YES"<<ln;
+}
+
+
+
+
+
+void Main() {
+    int t = 1;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cout << fixed << setprecision(1);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("errors.txt", "w", stderr);
+#endif
+    //--------------------
+        // int t;cin>>t;
+    Main();
+}
